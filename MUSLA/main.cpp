1b92d71828cfb94250f0640c73d63af259e97be6
June <kimjune01@gmail.com>
2016-07-21 09:39:51 -0700

    addinggitignore

---
+++ MUSLA/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MUSLA
+//
+//  Created by June Kim on 2016-07-21.
+//  Copyright © 2016 JuneKim_JimmyZhong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
