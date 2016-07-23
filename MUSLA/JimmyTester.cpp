f4a61d7c625acffd12458487651164446e2da490
Jimmy <jimmyz9627@gmail.com>
2016-07-23 15:45:26 -0700

    initalizing JimmyTester

---
+++ MUSLA/JimmyTester.cpp
@@ -0,0 +1,9 @@
+//
+//  JimmyTester.cpp
+//  MUSLA
+//
+//  Created by jimmy zhong on 2016-07-23.
+//  Copyright © 2016 JuneKim_JimmyZhong. All rights reserved.
+//
+
+#include "JimmyTester.hpp"
---
+++ MUSLA/JimmyTester.hpp
@@ -0,0 +1,14 @@
+//
+//  JimmyTester.hpp
+//  MUSLA
+//
+//  Created by jimmy zhong on 2016-07-23.
+//  Copyright © 2016 JuneKim_JimmyZhong. All rights reserved.
+//
+
+#ifndef JimmyTester_hpp
+#define JimmyTester_hpp
+
+#include <stdio.h>
+
+#endif /* JimmyTester_hpp */
